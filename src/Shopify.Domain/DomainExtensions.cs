9aaf137c0b0936197c7014faaf643a71bf8ea043
AlanEmersic <47435221+AlanEmersic@users.noreply.github.com>
2025-06-19 19:17:48 +0200

    Project setup (#1)

---
+++ src/Shopify.Domain/DomainExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Shopify.Domain;
+
+public static class DomainExtensions
+{
+    public static IServiceCollection AddDomain(this IServiceCollection services)
+    {
+        return services;
+    }
+}
