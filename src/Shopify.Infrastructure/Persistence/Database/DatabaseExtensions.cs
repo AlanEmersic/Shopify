352e239e3208952823dac8b50ae1f8c9d25073bd
Alan Emeršić <alan.emersic@gmail.com>
2025-06-21 12:53:26 +0200

    Database setup
    Added:
    - ShopifyDbContext
    - DatabaseInitializer for automatic migrations
    - Database configurations

---
+++ src/Shopify.Infrastructure/Persistence/Database/DatabaseExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shopify.Application.Common.Interfaces;
+
+namespace Shopify.Infrastructure.Persistence.Database;
+
+internal static class DatabaseExtensions
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        const string sectionName = "Database";
+        string? connectionString = configuration.GetConnectionString(sectionName);
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("Database connection string not found");
+        }
+
+        services.AddDbContext<ShopifyDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddHostedService<DatabaseInitializer>();
+
+        services.AddScoped<IUnitOfWork>(serviceProvider =>
+            serviceProvider.GetRequiredService<ShopifyDbContext>()
+        );
+
+        return services;
+    }
+}
---
+++ src/Shopify.Infrastructure/Persistence/Database/ShopifyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopify.Application.Common.Interfaces;
+
+namespace Shopify.Infrastructure.Persistence.Database;
+
+internal sealed class ShopifyDbContext : DbContext, IUnitOfWork
+{
+    public ShopifyDbContext(DbContextOptions<ShopifyDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ShopifyDbContext).Assembly);
+    }
+
+    public async Task CommitChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await SaveChangesAsync(cancellationToken);
+    }
+}
