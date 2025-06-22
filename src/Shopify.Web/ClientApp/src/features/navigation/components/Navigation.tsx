e946997ca296026953b6ab3a088a60361d15fefe
Alan Emeršić <alan.emersic@gmail.com>
2025-06-22 17:30:21 +0200

    Navigation page

---
+++ src/Shopify.Web/ClientApp/src/features/navigation/components/Navigation.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+
+import { NAVIGATION_ITEMS, ROUTES } from "features";
+
+function Navigation() {
+  return (
+    <nav className="border-gray-200 bg-white pb-[50px]">
+      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+        <div className="flex cursor-pointer items-center space-x-3 rtl:space-x-reverse">
+          <Link
+            to={ROUTES.HOME}
+            className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-cyan-700"
+            aria-current="page"
+          >
+            <span className="self-center text-2xl font-semibold whitespace-nowrap">Shopify</span>
+          </Link>
+        </div>
+
+        <div className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto">
+          <div className="mt-4 rounded-lg border border-gray-100 bg-gray-50 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse">
+            <div className="flex gap-10">
+              {NAVIGATION_ITEMS.map(item => (
+                <Link
+                  key={item.id}
+                  to={item.link}
+                  className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-cyan-700"
+                  aria-current="page"
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export { Navigation };
--- src/Shopify.Web/ClientApp/src/features/navigation/types/index.ts
+++ src/Shopify.Web/ClientApp/src/features/navigation/types/index.ts
@@ -2,3 +2,17 @@ export const ROUTES = {
   HOME: "/",
   PRODUCT_DETAILS: "/product/:id",
 };
+
+export type NavigationItem = {
+  id?: number;
+  name: "Home" | "About";
+  link: string;
+};
+
+export const NAVIGATION_ITEMS: NavigationItem[] = [
+  {
+    id: 1,
+    name: "Home",
+    link: ROUTES.HOME,
+  },
+];
