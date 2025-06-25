c8cc96d23dac020078e10cd0f04eebddede4b393
AlanEmersic <47435221+AlanEmersic@users.noreply.github.com>
2025-06-25 08:21:08 +0200

    Update JWT settings (#26)

--- src/Shopify.Infrastructure/Authentication/JwtToken/JwtSettings.cs
+++ src/Shopify.Infrastructure/Authentication/JwtToken/JwtSettings.cs
@@ -4,8 +4,8 @@ internal sealed class JwtSettings
 {
     public const string Section = "JwtSettings";
 
-    public string Audience { get; init; }
-    public string Issuer { get; init; }
-    public string Secret { get; init; }
+    public string Audience { get; init; } = null!;
+    public string Issuer { get; init; } = null!;
+    public string Secret { get; init; } = null!;
     public int TokenExpirationInMinutes { get; init; }
 }
